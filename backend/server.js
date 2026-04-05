11358533b23484a0227d5db0e153e984961d91f7
Mohit6577 <Mohitchoudhary657@gmail.com>
2026-04-05 14:07:43 +0530

    port updated

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is runing on port${PORT}`);
